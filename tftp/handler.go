fb321619ee9801d8ee9022656c6b0a4f85ee1b02
Andrew E. Bruno <aebruno2@buffalo.edu>
2022-11-10 13:45:45 +0000

    Support serving image assets over tftp

--- tftp/handler.go
+++ tftp/handler.go
@@ -21,17 +21,58 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/pin/tftp"
 	"github.com/ubccr/grendel/model"
 )
 
+func (s *Server) sendFile(fileName string, rf io.ReaderFrom) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Errorf("Failed to open %s: %s", fileName, err)
+		return err
+	}
+	n, err := rf.ReadFrom(file)
+	if err != nil {
+		log.Errorf("Failed to send %s via tftp: %s", fileName, err)
+		return err
+	}
+
+	log.Infof("Sent %s via tftp: %d bytes sent", fileName, n)
+	return nil
+}
+
+func (s *Server) imageFileHandler(filePath string, rf io.ReaderFrom) error {
+	imageName, fileType := filepath.Split(filePath)
+	bootImage, err := s.DB.LoadBootImage(strings.TrimSuffix(imageName, "/"))
+	if err != nil {
+		log.Errorf("File not found: %s", filePath)
+		return err
+	}
+
+	switch {
+	case fileType == "kernel":
+		return s.sendFile(bootImage.KernelPath, rf)
+	case strings.HasPrefix(fileType, "initrd-"):
+		i, err := strconv.Atoi(fileType[7:])
+		if err != nil || i < 0 || i >= len(bootImage.InitrdPaths) {
+			return fmt.Errorf("no initrd with ID %q", i)
+		}
+		initrd := bootImage.InitrdPaths[i]
+		return s.sendFile(initrd, rf)
+	}
+
+	return fmt.Errorf("File not found: %s", filePath)
+}
+
 func (s *Server) ReadHandler(token string, rf io.ReaderFrom) error {
 	fwtype, err := model.ParseFirmwareToken(token)
 	if err != nil {
-		log.Errorf("failed to parse token: %s", err)
-		return err
+		return s.imageFileHandler(token, rf)
 	}
 
 	log.Infof("Got read request for firmware type: %d", fwtype)
